84a8471f13be8834f2844041a0c6db7710f51189
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-11 01:01:03 +0200

    add: function to create a row of multiple cell in js with click event on each cell

--- static/main.js
+++ static/main.js
@@ -1,2 +1,20 @@
 alert("hello from js");
 
+function createGrid(height, width){
+	const gridContainer = document.querySelector(".grid_container");
+	for (let i = 1; i <= width; ++i){
+		let cell = document.createElement("div");
+		const baseTagId = "cell-";
+		cell.style.border = "1px solid black";
+		cell.style.display = "inline-block";
+		cell.style.width = "50px";
+		cell.style.height = "50px";
+		cell.id = baseTagId + i;
+		gridContainer.appendChild(cell);
+		cell.addEventListener("click", (e) => {
+			console.log(e.target.id);
+		});
+	}
+}
+
+createGrid(0,5);
