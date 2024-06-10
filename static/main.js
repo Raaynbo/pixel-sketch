a952f524213ea1eabef68546c9d59bddc78a2e96
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-11 01:24:23 +0200

    add: function can create a grid of X*Y, all cells have an id base on row and columns

--- static/main.js
+++ static/main.js
@@ -2,19 +2,26 @@ alert("hello from js");
 
 function createGrid(height, width){
 	const gridContainer = document.querySelector(".grid_container");
-	for (let i = 1; i <= width; ++i){
-		let cell = document.createElement("div");
-		const baseTagId = "cell-";
-		cell.style.border = "1px solid black";
-		cell.style.display = "inline-block";
-		cell.style.width = "50px";
-		cell.style.height = "50px";
-		cell.id = baseTagId + i;
-		gridContainer.appendChild(cell);
-		cell.addEventListener("click", (e) => {
-			console.log(e.target.id);
-		});
+	for ( let j = 1; j <= height; ++j){
+		let row = document.createElement('div');
+		row.style.margin = "0";
+		row.style.padding = "0";
+		row.style.height = "50px";
+		gridContainer.appendChild(row);
+		for (let i = 1; i <= width; ++i){
+			let cell = document.createElement("div");
+			const baseTagId = "cell-";
+			cell.style.border = "1px solid black";
+			cell.style.display = "inline-block";
+			cell.style.width = "50px";
+			cell.style.height = "50px";
+			cell.id = baseTagId + i + "-" + j;
+			row.appendChild(cell);
+			cell.addEventListener("click", (e) => {
+				console.log(e.target.id);
+			});
+		}
 	}
 }
 
-createGrid(0,5);
+createGrid(16,16);
