d866b6c865b86cc229aaf369a81354814abae4a0
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-11 01:49:54 +0200

    add: mouseover event added to each cell changing background color + a class name is added while generating each cell

--- static/main.js
+++ static/main.js
@@ -1,27 +1,34 @@
 alert("hello from js");
 
-function createGrid(height, width){
+function createGrid(height, width, cellHeight, cellWidth){
 	const gridContainer = document.querySelector(".grid_container");
 	for ( let j = 1; j <= height; ++j){
 		let row = document.createElement('div');
 		row.style.margin = "0";
 		row.style.padding = "0";
-		row.style.height = "50px";
+		row.style.display = "flex";
+		row.style.height = cellHeight;
 		gridContainer.appendChild(row);
 		for (let i = 1; i <= width; ++i){
 			let cell = document.createElement("div");
 			const baseTagId = "cell-";
 			cell.style.border = "1px solid black";
-			cell.style.display = "inline-block";
-			cell.style.width = "50px";
-			cell.style.height = "50px";
+			cell.style.width = cellWidth;
+			cell.style.height = cellHeight;
 			cell.id = baseTagId + i + "-" + j;
+			cell.classList.add("cell");
 			row.appendChild(cell);
 			cell.addEventListener("click", (e) => {
-				console.log(e.target.id);
 			});
+			cell.addEventListener("mouseover", (e) => hoverTrail(e));
 		}
 	}
 }
 
-createGrid(16,16);
+function hoverTrail(event) {
+	event.target.style.backgroundColor = "blue";
+}
+
+
+const cellH = "20px" ;
+createGrid(16,16, cellH, cellH);
