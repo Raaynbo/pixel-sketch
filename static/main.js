17a1e3bd6394040ee7f43873a42eb36e11a92a62
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-11 18:42:03 +0200

    add: added edition mode, added random color mode

--- static/main.js
+++ static/main.js
@@ -1,26 +1,57 @@
-alert("hello from js");
+let colorMode = 0;
+let editionMode = 0; //0 = hover - 1 = click
+const cellH = "20px" ;
+const colorBoolBtn = document.querySelector("#randomColor");
+const editionBtn = document.querySelector("#editionMode");
+const colorParam = document.querySelector("#colorLabel");
+createGrid(16,16, cellH, cellH);
+const resetBtn = document.querySelector(".reset_grid");
+resetBtn.addEventListener("click", () => {
+	const allGridCell = document.querySelectorAll(".cell");
+	allGridCell.forEach((cell) => {
+		cell.style.backgroundColor = "white";
+	});
+});
+
+const gridCells = document.querySelectorAll(".cell");
+gridCells.forEach((cell) => {
+	cell.addEventListener("mouseover", (e) => {
+		if (editionMode == false){
+			if(colorMode === 0){
+				hoverTrail(e);
+			}else{
+				const colorSelected = document.querySelector("#color");
+				hoverTrail(e, colorSelected.value);
+			}
+		}
+	});
+	cell.addEventListener("click", (e) => {
+		if (editionMode == true){
+			if(colorMode === 0){
+				hoverTrail(e);
+			}else{
+				const colorSelected = document.querySelector("#color");
+				hoverTrail(e, colorSelected.value);
+			}
+		}
+	});
+});
 
 function createGrid(height, width, cellHeight, cellWidth){
 	const gridContainer = document.querySelector(".grid_container");
 	for ( let j = 1; j <= height; ++j){
 		let row = document.createElement('div');
-		row.style.margin = "0";
-		row.style.padding = "0";
-		row.style.display = "flex";
+		row.classList.add("row"); 
 		row.style.height = cellHeight;
 		gridContainer.appendChild(row);
 		for (let i = 1; i <= width; ++i){
 			let cell = document.createElement("div");
 			const baseTagId = "cell-";
-			cell.style.border = "1px solid black";
 			cell.style.width = cellWidth;
 			cell.style.height = cellHeight;
 			cell.id = baseTagId + i + "-" + j;
 			cell.classList.add("cell");
 			row.appendChild(cell);
-			cell.addEventListener("click", (e) => {
-			});
-			cell.addEventListener("mouseover", (e) => hoverTrail(e));
 		}
 	}
 }
@@ -32,19 +63,31 @@ function generateRandomColor(){
 	return "rgb(" + r + "," + g + "," + b + ")";
 }
 
-function hoverTrail(event) {
-	let color = generateRandomColor();
-	console.log(color);
+function hoverTrail(event, color=generateRandomColor()) {
 	event.target.style.backgroundColor =`${color}`;
 }
 
 
-const cellH = "20px" ;
-createGrid(16,16, cellH, cellH);
-const resetBtn = document.querySelector(".reset_grid");
-resetBtn.addEventListener("click", () => {
-	const allGridCell = document.querySelectorAll(".cell");
-	allGridCell.forEach((cell) => {
-		cell.style.backgroundColor = "white";
-	});
-});
+
+function switchColorMode(){
+	if(colorMode === 0){
+	colorMode = 1;
+	colorParam.style.display = "block";
+	colorBoolBtn.textContent = "No";
+}else {
+	colorMode = 0;
+	colorBoolBtn.textContent = "Yes";
+	colorParam.style.display = "none";
+	}
+}
+
+function switchEditionMode(){
+	if(editionMode===0){
+		editionMode = 1;
+		editionBtn.textContent = "Click to modify"
+	}else{
+		
+		editionMode = 0;
+		editionBtn.textContent = "Hover to modify"
+	}
+}
