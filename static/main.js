fe91d77c49a16b0da49260a89bde591ad9103fae
Raaynbo <rayan.boudjema@protonmail.com>
2024-06-13 08:20:07 +0200

    refactor: changed function createGrid to receive the number of desired cell in a row and a grid dimension + adjusted intern processing

--- static/main.js
+++ static/main.js
@@ -1,17 +1,26 @@
-let colorMode = 0;
+// variable
+let colorMode = 0; // 0 = random - 1 = defined color
 let editionMode = 0; //0 = hover - 1 = click
-const cellH = "20px" ;
+const gridSize = 980 ; // = base size (width and height)
+
+// DOM element
 const colorBoolBtn = document.querySelector("#randomColor");
 const editionBtn = document.querySelector("#editionMode");
 const colorParam = document.querySelector("#colorLabel");
 const resizeBtn = document.querySelector("#resizeBtn");
 const spanClose = document.querySelector(".close");
-createGrid(16,16, cellH, cellH);
 const resetBtn = document.querySelector(".reset_grid");
 const resizeModal = document.querySelector("#resize_modal");
 const divResizeDemo = document.querySelector("#resize_demo");
 const cellInputDimension = document.querySelector("#cellInputDimension");
 
+// base grid creation
+createGrid(16, gridSize);
+
+// creation of all the event attached to DOM element
+// ----------------------------------------------------
+//
+// reset the color of all the cells in the grid
 resetBtn.addEventListener("click", () => {
 	const allGridCell = document.querySelectorAll(".cell");
 	allGridCell.forEach((cell) => {
@@ -19,14 +28,17 @@ resetBtn.addEventListener("click", () => {
 	});
 });
 
+//close the resize modal 
 spanClose.addEventListener("click", () => {
 	resizeModal.style.display = "none";
 });
 
+//display the resize modal
 resizeBtn.addEventListener("click", (e) => {
 	resizeModal.style.display = "block";
 });
 
+//display the demo cell inside the resize modal
 cellInputDimension.addEventListener("keyup", (e) => {
 	let demoDiv = document.querySelector("#demo_div");
 	if (Number(cellInputDimension.value > 100)){
@@ -39,6 +51,7 @@ cellInputDimension.addEventListener("keyup", (e) => {
 	demoDiv.style.width = cellInputDimension.value + "px";
 	demoDiv.style.height = cellInputDimension.value + "px";
 });
+
 
 resizeBtn.addEventListener("clicki", (e) => {
 	let newSize = 0;
@@ -54,45 +67,51 @@ resizeBtn.addEventListener("clicki", (e) => {
 	while(gridContainer.firstChild){
 		gridContainer.removeChild(gridContainer.lastChild);
 	}
-	createGrid(newSize, newSize,cellH, cellH);
+	createGrid(newSize,cellH);
 
 });
 
+// ------------------ Function definition --------------
+// add Events to a cell 
 function setEventOnCells(cell){
 	cell.addEventListener("mouseover", (e) => {
 		if (editionMode == false){
 			if(colorMode === 0){
-				hoverTrail(e);
+				hoverTrail(e.target);
 			}else{
 				const colorSelected = document.querySelector("#color");
-				hoverTrail(e, colorSelected.value);
+				hoverTrail(e.target, colorSelected.value);
 			}
 		}
 	});
 	cell.addEventListener("click", (e) => {
 		if (editionMode == true){
 			if(colorMode === 0){
-				hoverTrail(e);
+				hoverTrail(e.target);
 			}else{
 				const colorSelected = document.querySelector("#color");
-				hoverTrail(e, colorSelected.value);
+				hoverTrail(e.target, colorSelected.value);
 			}
 		}
 	});
 }
 
-function createGrid(height, width, cellHeight, cellWidth){
+// create a grid of height*width cells, each cell have a width and height of cellDim
+function createGrid(cellNb, gridDim){
 	const gridContainer = document.querySelector(".grid_container");
-	for ( let j = 1; j <= height; ++j){
+	gridContainer.style.width = gridDim + "px";
+	gridContainer.style.height = gridDim + "px";
+	cellDim = (gridDim/cellNb) + "px";
+	for ( let j = 1; j <= cellNb; ++j){
 		let row = document.createElement('div');
 		row.classList.add("row"); 
-		row.style.height = cellHeight;
+		row.style.height = cellDim;
 		gridContainer.appendChild(row);
-		for (let i = 1; i <= width; ++i){
+		for (let i = 1; i <= cellNb; ++i){
 			let cell = document.createElement("div");
 			const baseTagId = "cell-";
-			cell.style.width = cellWidth;
-			cell.style.height = cellHeight;
+			cell.style.width = cellDim;
+			cell.style.height = cellDim;
 			cell.id = baseTagId + i + "-" + j;
 			cell.classList.add("cell");
 			row.appendChild(cell);
@@ -101,6 +120,7 @@ function createGrid(height, width, cellHeight, cellWidth){
 	}
 }
 
+// generate a rgb color code
 function generateRandomColor(){
 	let r = Math.floor(Math.random()*256);
 	let g = Math.floor(Math.random()*256);
@@ -108,12 +128,13 @@ function generateRandomColor(){
 	return "rgb(" + r + "," + g + "," + b + ")";
 }
 
-function hoverTrail(event, color=generateRandomColor()) {
-	event.target.style.backgroundColor =`${color}`;
+// change cell color, need to pass the cell DOM element and a color in rgb or hex, if none generate a random color
+function hoverTrail(cell, color=generateRandomColor()) {
+	cell.style.backgroundColor =`${color}`;
 }
 
 
-
+// change the color mode
 function switchColorMode(){
 	if(colorMode === 0){
 	colorMode = 1;
@@ -126,6 +147,7 @@ function switchColorMode(){
 	}
 }
 
+// change edition mode
 function switchEditionMode(){
 	if(editionMode===0){
 		editionMode = 1;
